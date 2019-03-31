de3cc82ad506b8a05f375cb99d66743f620cf7fc
ju851bah <julian@bahner.de>
2019-03-31 16:21:32 +0200

    added player test to trigger builds

--- src/test/scala/de/htwg/se/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/PlayerSpec.scala
@@ -2,4 +2,15 @@ package de.htwg.se
 import org.scalatest._
 
 class PlayerSpec extends WordSpec with Matchers {
+  "A Player" when {
+    "new" should {
+      val player = new Player("name", 1500)
+      "have a name" in {
+        player.name should be("name")
+      }
+      "have money" in {
+        player.money should be(1500)
+      }
+    }
+  }
 }
