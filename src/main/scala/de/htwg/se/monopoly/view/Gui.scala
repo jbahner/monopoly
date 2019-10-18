d99c5aa6b653dca58f1796a02b24ed20179e03da
Nik <ni851ste@htwg-konstanz.de>
2019-10-18 09:20:54 +0200

    initial Gui Commit

--- src/main/scala/de/htwg/se/monopoly/view/Gui.scala
+++ src/main/scala/de/htwg/se/monopoly/view/Gui.scala
@@ -1,5 +1,12 @@
 package de.htwg.se.monopoly.view
 
-class Gui {
+import de.htwg.se.monopoly.controller.Controller
+import de.htwg.se.monopoly.controller.GameStatus.GameStatus
+import de.htwg.se.monopoly.util.Observer
 
+class Gui(controller: Controller) extends Observer {
+  override def update(gameStatus: GameStatus): Unit = {
+
+    
+  }
 }
