21d2f790219db5e42ab11da0094c0a2d4ff55747
Nik Steinbrügge <ni851ste@htwg-konstanz.de>
2019-12-11 22:24:42 +0100

    More detailed Gui + first picture, picture needs work

--- src/main/scala/de/htwg/se/monopoly/view/Gui.scala
+++ src/main/scala/de/htwg/se/monopoly/view/Gui.scala
@@ -3,33 +3,27 @@ package de.htwg.se.monopoly.view
 import java.awt.Color
 import java.util
 
-import com.sun.source.tree.BinaryTree
 import de.htwg.se.monopoly.controller.{Controller, GameStatus, UpdateGui, UpdateInfo}
-import de.htwg.se.monopoly.model.boardComponent.{Buyable, Street}
-import javax.swing.BorderFactory
-import javax.swing.border.Border
+import de.htwg.se.monopoly.model.boardComponent.{Building, Field, Street}
+import javax.imageio.ImageIO
+import javax.swing.{BorderFactory, ImageIcon, JLabel}
 
-import scala.collection.{SortedSet, mutable}
-import scala.swing.Swing.LineBorder
+import scala.reflect.io.File
 import scala.swing._
 import scala.swing.event._
-import scala.util.Try
 
 class Gui(controller: Controller) extends Frame {
 
     //TODO
     // - show how much houses are on which street
     // - wrap text output in own labels per line to make mupltiple lines in gui passible
+    // - make auto setup an extra command -- set up game with custom player names and -count
 
-    val windowDimension = new Dimension(700, 300)
+    val windowDimension = new Dimension(700, 350)
     val menuBarDimension = new Dimension(1000, 30)
 
     listenTo(controller)
     title = "Monopoly+"
-
-    var infoPanel = new GridPanel(3, 1)
-    var buttonPanel = new FlowPanel
-    var textPanel = new FlowPanel
 
     minimumSize = windowDimension
 
@@ -39,17 +33,15 @@ class Gui(controller: Controller) extends Frame {
     reactions += {
         case event: UpdateInfo => redraw()
         case event: UpdateGui => redraw()
-
     }
 
     def redraw(): Unit = {
         contents = new BorderPanel {
+            add(generateCenterPanel(), BorderPanel.Position.Center)
             add(createMenuBar(), BorderPanel.Position.North)
-            add(generateTextPanel(), BorderPanel.Position.Center)
+            add(generateBuildButtons(), BorderPanel.Position.East)
             add(redrawButtons(), BorderPanel.Position.South)
             add(generateLeftPanel(), BorderPanel.Position.West)
-            add(generateBuildButtons(), BorderPanel.Position.East)
-            //repaint
         }
     }
 
@@ -57,7 +49,7 @@ class Gui(controller: Controller) extends Frame {
         new MenuBar {
             contents += new Menu("File") {
                 contents += new MenuItem(Action("New Game") {
-                   controller.setUp()
+                    controller.setUp()
                 })
                 contents += new MenuItem(Action("Load") {
                     print("Not implemented yet")
@@ -129,10 +121,65 @@ class Gui(controller: Controller) extends Frame {
 
     }
 
+    def generateCenterPanel(): GridPanel = {
+        new GridPanel(1, 2) {
+            contents += generateTextPanel()
+            contents += generateCenterCurrentFieldDetails()
+        }
+    }
+
+    def generateCenterCurrentFieldDetails(): GridPanel = {
+        new GridPanel(7, 1) {
+
+            contents += new Label("Current Field") {
+                font = (new Font(font.getFontName, font.getStyle, 20))
+                background = Color.CYAN
+                opaque = true
+            }
+
+            val curField: Field = controller.getCurrentField
+            contents += new Label(curField.getName)
+
+
+            curField match {
+                case street: Street =>
+                    contents += new Label(
+                        if (curField.asInstanceOf[Street].isBought) "Bought by " + controller.getBuyer(curField.asInstanceOf[Street]).get.name
+                        else "Not owned")
+                    contents += new Label("Houses: " + curField.asInstanceOf[Street].numHouses.toString)
+                    contents += new Label("House price: " + curField.asInstanceOf[Street].houseCost)
+                    contents += new Label("Current Rent: " + curField.asInstanceOf[Street].getRent())
+                case building: Building =>
+                    contents += new Label(
+                        if (curField.asInstanceOf[Building].isBought) "Bought by " + controller.getBuyer(curField.asInstanceOf[Building]).get.name
+                        else "Not owned")
+                case _ =>
+            }
+            if (curField.getName.equals("Go")) {
+                contents += new Label() {
+                    icon = new ImageIcon("D:\\HTWG\\5.Semester\\monopoly\\src\\main\\scala\\de\\htwg\\se\\monopoly\\view\\textures\\go_field.png")
+                    maximumSize = new Dimension(100,100)
+                }
+            }
+
+
+            border = BorderFactory.createLineBorder(Color.black)
+
+            //var curFieldStreet
+            //contents += new Label ("Houses: " + curField.asInstanceOf[Street].numHouses)
+            //contents += new Label ("Bought??: " + curField.asInstanceOf[Street].isBought)
+        }
+    }
+
     def generateTextPanel(): GridPanel = {
         val currentMsg = getCurrentGameMessage().split("\n")
 
-        new GridPanel(currentMsg.size, 1) {
+        new GridPanel(7, 1) {
+            contents += new Label("Game Info") {
+                font = (new Font(font.getFontName, font.getStyle, 20))
+                background = Color.CYAN
+                opaque = true
+            }
             currentMsg.foreach(cutMsg => this.contents += new Label(cutMsg))
             border = BorderFactory.createLineBorder(Color.black)
         }
@@ -145,17 +192,17 @@ class Gui(controller: Controller) extends Frame {
             contents += new Label("  " + controller.getCurrentPlayer().get.name + "    ")
             contents += new Label(controller.getCurrentPlayer().get.money + " €")
             contents += new Label("")
-            contents += new Label(controller.getCurrentField.getName)
         }
     }
 
     def getCurrentGameMessage(): String = {
         controller.controllerState match {
-            case GameStatus.START_OF_TURN => "  It is your start of the turn!\nRoll the dice.  "
+            case GameStatus.START_OF_TURN => "  " + controller.getCurrentPlayer().get.name + "'s turn.\nIt is your start of the turn!\nRoll the dice.  "
             case GameStatus.CAN_BUILD =>
                 controller.buildStatus match {
                     case GameStatus.BuildStatus.DEFAULT => "  Your rolled a " + controller.currentDice + ".\nYour new Field is " + controller.getCurrentField.getName + ".  "
                     case GameStatus.BuildStatus.BUILT => "  Sucessfully build house.  "
+                    case _ => "  Un catched BuildStatus  "
                 }
 
             case _ => "------ ERROR ------"
