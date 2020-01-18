331e03a5c103958d86532aeb0af59312bdd7fce3
Nik Steinbruegge <ni851ste@htwg-konstanz.de>
2020-01-18 15:30:32 +0100

    Start of Load Xml

--- src/main/scala/de/htwg/se/monopoly/controller/GameStatus.scala
+++ src/main/scala/de/htwg/se/monopoly/controller/GameStatus.scala
@@ -11,11 +11,39 @@ object GameStatus extends Enumeration {
         ALREADY_BOUGHT -> "Already owning the field",
     )
 
+    val revMap: Map[String, GameStatus] = Map[String, GameStatus](
+
+        "START_OF_TURN" ->START_OF_TURN,
+        "ROLLED" ->ROLLED,
+        "NEW_FIELD" -> NEW_FIELD,
+        "NEXT_PLAYER" -> NEXT_PLAYER,
+        "BOUGHT_BY_OTHER" -> BOUGHT_BY_OTHER,
+        "CAN_BUY" -> CAN_BUY,
+        "ALREADY_BOUGHT" -> ALREADY_BOUGHT,
+        "BOUGHT" -> BOUGHT,
+        "NOTHING" -> NOTHING,
+        "PASSED_GO" -> PASSED_GO,
+        "MISSING_MONEY" -> MISSING_MONEY,
+        "CAN_BUILD" -> CAN_BUILD,
+        "DONE" -> DONE
+    )
+
+
     def message(gameStatus: GameStatus): String = map(gameStatus)
 
     object BuildStatus extends Enumeration {
         type BuildStatus = Value
         val DEFAULT, DONE, INVALID_ARGS, NOT_OWN, MISSING_MONEY, BUILT, TOO_MANY_HOUSES = Value
+
+        val revMap: Map[String, BuildStatus] = Map[String, BuildStatus](
+            "DEFAULT" -> DEFAULT,
+            "DONE" -> DONE,
+            "INVALID_ARGS" -> INVALID_ARGS,
+            "NOT_OWN" -> NOT_OWN,
+            "MISSING_MONEY" -> MISSING_MONEY,
+            "BUILT" -> BUILT,
+            "TOO_MANY_HOUSES" -> TOO_MANY_HOUSES
+        )
     }
 
 }
--- src/main/scala/de/htwg/se/monopoly/util/fileIo/fileIoXml/FileIoXml.scala
+++ src/main/scala/de/htwg/se/monopoly/util/fileIo/fileIoXml/FileIoXml.scala
@@ -1,12 +1,25 @@
 package de.htwg.se.monopoly.util.fileIo.fileIoXml
 
-import de.htwg.se.monopoly.controller.IController
+import com.google.inject.Guice
+import de.htwg.se.monopoly.MonopolyModule
+import de.htwg.se.monopoly.controller.{GameStatus, IController}
+import de.htwg.se.monopoly.model.boardComponent.boardBaseImpl.Board
 import de.htwg.se.monopoly.util.fileIo.IFileIo
 
 import scala.xml.PrettyPrinter
 
 class FileIoXml extends IFileIo {
-    def load: IController = ???
+    def load: IController = {
+        val controller = Guice.createInjector(new MonopolyModule).getInstance(classOf[IController])
+        val file = scala.xml.XML.loadFile("save-game.xml")
+        controller.controllerState = GameStatus.revMap((file \\ "controller" \ "game-status").text)
+        controller.buildStatus = GameStatus.BuildStatus.revMap((file \\ "controller" \ "build-status").text)
+        //controller.setBoard(Board())
+
+
+
+        controller
+    }
 
     def saveString(controller: IController): Unit = {
         import java.io._
