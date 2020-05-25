ca87b2b78736c29b16a74062c277d7f0455e41a1
Nik Steinbruegge <ni851ste@htwg-konstanz.de>
2020-05-25 16:25:54 +0200

    Readded akka dependency

--- uiAndControllerModule/src/main/scala/monopoly/WebServer.scala
+++ uiAndControllerModule/src/main/scala/monopoly/WebServer.scala
@@ -24,6 +24,8 @@ object WebServer {
 
         val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
+
+        // Server Shutdown
         println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
         StdIn.readLine() // let it run until user presses return
         bindingFuture
