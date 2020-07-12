60ff25df8cf6063b179ca4929129777d79c6c9b4
Nik Steinbruegge <ni851ste@htwg-konstanz.de>
2020-07-12 10:03:57 +0200

    Added Performance Tests

---
+++ performanceTests/H2v2/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'Active Users',
+data: [
+  [1592916012000,1],[1592916013000,1],[1592916014000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
